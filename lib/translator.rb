a4918564330fcdda9407f92a8f3b9e07cfbe18ee
Alexandra Pestruyeva <apestruy@gmail.com>
2019-12-06 04:40:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,7 @@ def get_japanese_emoticon(file_path, emoticon)
   if load_library(file_path)[get_emoticon][emoticon]
     return load_library(file_path[get_emoticon][emoticon])
   else 
-    return 
+    return "Sorry, that emoticon was not found"
   end 
 end
 
